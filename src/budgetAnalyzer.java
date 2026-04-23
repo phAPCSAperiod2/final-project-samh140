3dd457ece6c86345efd03691e3965a187b610ea5
samh140 <residingblue@gmail.com>
2026-04-23 17:11:16 +0000

    Basic methods to give feedback for each category using comparing methods ( in same class )

--- src/budgetAnalyzer.java
+++ src/budgetAnalyzer.java
@@ -1,6 +1,6 @@
 public class budgetAnalyzer {
 
-    
+
     public boolean compareNeeds(Day day) {
         if (day.getDailyNeedsBudget() > day.getSpendingForNeeds()) {
             return true;
@@ -29,4 +29,44 @@ public class budgetAnalyzer {
     public double calculateTotalSpending(Day day) {
         return day.getSpendingForNeeds() + day.getSpendingForWants() + day.getSpendingForSavings();
     }
+
+
+
+
+    // feedback for one day
+    public void getNeedsFeedback(Day day) {
+        if (compareNeeds(day))
+        {
+            System.out.println("");
+        }
+        else {
+            System.out.println("");
+        }
+
+    }
+
+    public void getWantsFeedback(Day day) {
+        if (compareWants(day))
+        {
+            System.out.println("");
+        }
+        else {
+            System.out.println("");
+        }
+
+    }
+
+    public void getSavingsFeedback(Day day) {
+        if (compareSavings(day))
+        {
+            System.out.println("");
+        }
+        else {
+            System.out.println("");
+        }
+
+    }
+
+    // I want a method that generalizes the data for the two weeks.
+    // feedback generalized to two weeks
 }
