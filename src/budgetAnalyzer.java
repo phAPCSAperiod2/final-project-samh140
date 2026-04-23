d4372c2782406cae4772a5114a34f5ad4a9bbaf8
samh140 <residingblue@gmail.com>
2026-04-23 06:24:23 +0000

    Added methods that compare daily budget and the actaul spending for each category.
    return true if spending was less than budget, false otherwise
    
    reutnr true if saving was greater than savings budget, false otherwise
    
    created calculateTotalSpending method

--- src/budgetAnalyzer.java
+++ src/budgetAnalyzer.java
@@ -1,16 +1,32 @@
-public class budgetAnalyzer
-{
+public class budgetAnalyzer {
 
-    private double twoWeekIncome;
+    
+    public boolean compareNeeds(Day day) {
+        if (day.getDailyNeedsBudget() > day.getSpendingForNeeds()) {
+            return true;
+        }
 
+        return false;
+    }
 
+    public boolean compareWants(Day day) {
+        if (day.getDailyWantsBudget() > day.getSpendingForWants()) {
+            return true;
+        }
 
-    public boolean compareSpending() {
-        
+        return false;
+    }
 
+    public boolean compareSavings(Day day) {
+        if (day.getDailySavingsBudget() <= day.getSpendingForSavings()) {
+            return true;
+        }
 
         return false;
     }
 
 
+    public double calculateTotalSpending(Day day) {
+        return day.getSpendingForNeeds() + day.getSpendingForWants() + day.getSpendingForSavings();
+    }
 }
