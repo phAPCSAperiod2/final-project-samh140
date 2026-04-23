225e83bce58df510ba5209b3bb6fc784860e7d41
samh140 <residingblue@gmail.com>
2026-04-23 17:34:32 +0000

    Tested the method I created in tester. Must use instance of budgetAnalyzer, I wonder if I should turn them into static methods instead.

--- src/calendarTester.java
+++ src/calendarTester.java
@@ -2,8 +2,12 @@ public class calendarTester
 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        Day day = new Day(0.5, 0.5, 0.2);
+        Day day = new Day(0.5, 0.5, 0.2, 500);
         System.out.println(day.toString());
+
+        // instance of budgetAnalyzer
+        budgetAnalyzer analyzer = new budgetAnalyzer();
+        analyzer.getNeedsFeedback(day);
     }
 }
 
