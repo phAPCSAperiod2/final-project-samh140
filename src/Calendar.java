f7f038c97ea7ee6c7fb5020e79c86a4b1a2aded9
samh140 <residingblue@gmail.com>
2026-04-30 00:53:45 +0000

    SPRINT 3 DAY 1, added viewDay method that is used to view a specific day and a method that checks the day number.

--- src/Calendar.java
+++ src/Calendar.java
@@ -3,6 +3,8 @@ public class Calendar
     private Day[][] calendar;
     private final int WEEKS = 2;
     private final int DAYS_PER_WEEK = 7;
+    private final int MINIMUM_DAY_NUMBER = 1;
+    private final int MAXIMUM_DAY_NUMBER = 14;
 
 
     public Calendar(double needsProp, double wantsProp, double savingsProp, double twoWeekIncome)
@@ -19,14 +21,34 @@ public class Calendar
 
     }
 
+    public boolean isValidDayNumber(int dayNumber)
+    {
+        return dayNumber >= MINIMUM_DAY_NUMBER && dayNumber <= MAXIMUM_DAY_NUMBER;
+    }
+
     public Day getDay(int dayNumber)
     {
+        if (!isValidDayNumber(dayNumber))
+        {
+            return null;
+        }
+
         int row = (dayNumber - 1)/7;
         int column = (dayNumber - 1) % 7;
         return calendar[row][column];
     }
 
+    public String viewDay(int dayNumber)
+    {
+        Day day = getDay(dayNumber);
 
+        if(day == null)
+        {
+            return "Invalid day number.";
+        }
+
+        return day.toString();
+    }
 
 
 }
