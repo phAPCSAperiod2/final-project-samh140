c2c01ca2889c72449cac803cb5ba7a09e5d8bda4
samh140 <residingblue@gmail.com>
2026-04-30 00:21:34 +0000

    DAY 3 SPRINT 1 Created calendar that contains day objects with each day having the same budget. Create getDay method

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,19 +1,32 @@
 public class Calendar
 {
-    // uses "day" objects to create an arrayList
+    private Day[][] calendar;
+    private final int WEEKS = 2;
+    private final int DAYS_PER_WEEK = 7;
 
 
-    // need to be 2D array, will contain day objects
-    public day[][] calendar;
-
-
-    public calendar()
+    public Calendar(double needsProp, double wantsProp, double savingsProp, double twoWeekIncome)
     {
-        // creates a calendar of 2 weeks
+        calendar = new Day[WEEKS][DAYS_PER_WEEK];
 
-        this.calendar = new calendar[2][7];
-
+        for (int row = 0; row < calendar.length; row++)
+        {
+            for (int column = 0; column < calendar[row].length; column++)
+            {
+                calendar[row][column] = new Day(needsProp, wantsProp, savingsProp, twoWeekIncome);
+            }
+        }
 
     }
+
+    public Day getDay(int dayNumber)
+    {
+        int row = (dayNumber - 1)/7;
+        int column = (dayNumber - 1) % 7;
+        return calendar[row][column];
+    }
+
+
+
 
 }
